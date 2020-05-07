b0533f602fed0afc30eba2a4f2f59ca1f9a6e312
rhouse2020 <rhouseinthemiddleofrstreet@gmail.com>
2020-05-07 17:41:40 -0400

    initial code in R to use stan model

---
+++ modeling.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(rstan)
+
+va_data <- read.csv('va_daily.csv')
+va_data$Report.Date <- as.Date(as.character(va_data$Report.Date), '%m/%d/%Y')
+va_data <- va_data %>% arrange(Report.Date)
+
+va_districts <- va_data %>% group_by(Report.Date, VDH.Health.District) %>% 
+    summarise(Total.Cases=sum(Total.Cases), 
+              Hospitalizations=sum(Hospitalizations), 
+              Deaths=sum(Deaths))
+va_districts <- va_districts %>% group_by(VDH.Health.District) %>%
+    mutate(day_change=Total.Cases -lag(Total.Cases), day_inc=day_change>lag(day_change))
+
+fairfax <- va_districts %>% filter(VDH.Health.District == "Fairfax")
+
+data <- list('T'=length(fairfax$Total.Cases), 'Y'=log(fairfax$Total.Cases))
+
+model_file <- stan_model('local_lvl_irw.stan')
+
+stan_fit <- sampling(model_file, data, iter = 2000, chains = 4)
