a649342c9b074af6b4e2f10a906bc1f55c5947c9
rhouse2020 <rhouseinthemiddleofrstreet@gmail.com>
2020-05-26 11:37:00 -0400

    trends 0526, update to include hosp and dead trends

--- dow_model.R
+++ dow_model.R
@@ -6,27 +6,27 @@ va_data <- read_csv(filename ,
                                      'Total Cases' = col_integer(),
                                      'Hospitalizations' = col_integer(),
                                      'Deaths' = col_integer()))
-# renaming columns
-va_data <- va_data %>% 
-    select(date=`Report Date`, Locality, district=`VDH Health District`, cases=`Total Cases`, Hospitalizations, Deaths) %>% 
-    arrange(date)
 
 # Using Locality instead of health districts to pull out just a single county
 va_locality <- va_data %>% 
-    select(date, Locality, tot_cases=cases, hosp=Hospitalizations, deaths=Deaths)
+    select(date=`Report Date`, Locality, tot_cases=`Total Cases`, hosp=Hospitalizations, deaths=Deaths) %>% 
+    arrange(date)
 
 va_locality <- va_locality %>% group_by(Locality) %>% 
-    mutate(new_cases=tot_cases -lag(tot_cases)) %>% # get the count of daily new cases
+    mutate(new_cases=tot_cases -lag(tot_cases), # get the count of daily new cases
+           new_hosp = hosp - lag(hosp,1),       # get the count of daily new hospitalizations
+           new_dead = deaths - lag(deaths,1)) %>% 
     filter(!(is.na(new_cases))) # removing that first date where we don't know the "new" number
 
-
 fairfax <- va_locality %>% filter(Locality == "Fairfax")
+fairfax$new_hosp[fairfax$new_hosp < 0] <- 0 # b/c a coupele of them are negative
 
 # Data for the model
 data <- list(
     'T'=length(fairfax$new_cases), # number of cases
     'S'=7, # days in a week
-    'INTR'=51,
+    'K'=2,
+    'INTR'=c(51,68),
     'Y'=log(1 + fairfax$new_cases)) # log(# cases + 1)
 
 # Compiling the model
@@ -40,11 +40,56 @@ model_results <- extract(stan_fit)
 
 # since we did log(new_cases + 1), subtracting the 1 here
 trend <- apply(model_results$mu_exp, 2, mean) - 1
-
 trend <- data.frame('date'=fairfax$date,'trend'=trend)
-
-write.csv(trend, 'trends/trend_today_dow.csv', row.names = FALSE)
 
 ggplot(fairfax, aes(x=date, y=new_cases)) + geom_point() + 
     labs(title='Daily change in # of Covid-19 cases for Fairfax') + xlab('Date') + ylab('Daily Delta') + 
     geom_line(aes(fairfax$date, y=trend$trend), color='blue')
+
+################################
+# Model without intervention dates for Hospitalizations and Deaths
+
+model_nointr_file <- stan_model('lcl_lvl_irw_dow_no_intr.stan')
+
+# Hospitalizations
+data_hosp <-list(
+    'T'=length(fairfax$new_hosp), # number of cases
+    'S'=7, # days in a week
+    'Y'=log(1 + fairfax$new_hosp)) # b/c sometimes it is negative
+
+stan_fit_hosp <- sampling(model_nointr_file, data_hosp, iter = 800, chains = 2,
+                          control=list(max_treedepth=13, adapt_delta=0.98))
+
+model_results_hosp <- extract(stan_fit_hosp)
+
+# since we did log(new_cases + 1), subtracting the 1 here
+trend_hosp <- apply(model_results_hosp$mu_exp, 2, mean) - 1
+trend_hosp <- data.frame('date'=fairfax$date,'trend_hosp'=trend_hosp)
+
+ggplot(fairfax, aes(x=date, y=new_hosp)) + geom_point() + 
+    labs(title='Daily change in # of Covid-19 cases for Fairfax') + xlab('Date') + ylab('Daily Delta') + 
+    geom_line(aes(fairfax$date, y=trend_hosp$trend_hosp), color='blue')
+
+# Deaths
+data_dead <-list(
+    'T'=length(fairfax$new_dead), # number of cases
+    'S'=7, # days in a week
+    'Y'=log(1 + fairfax$new_dead)) # b/c sometimes it is negative
+
+stan_fit_dead <- sampling(model_nointr_file, data_dead, iter = 800, chains = 2,
+                          control=list(max_treedepth=13, adapt_delta=0.98))
+
+model_results_dead <- extract(stan_fit_dead)
+
+# since we did log(new_cases + 1), subtracting the 1 here
+trend_dead <- apply(model_results_dead$mu_exp, 2, mean) - 1
+trend_dead <- data.frame('date'=fairfax$date,'trend_dead'=trend_dead)
+
+ggplot(fairfax, aes(x=date, y=new_dead)) + geom_point() + 
+    labs(title='Daily change in # of Covid-19 cases for Fairfax') + xlab('Date') + ylab('Daily Delta') + 
+    geom_line(aes(fairfax$date, y=trend_dead$trend_dead), color='blue')
+
+all_trends <- left_join(trend, trend_hosp, by='date')
+all_trends <- left_join(all_trends, trend_dead, by='date')
+
+write.csv(all_trends, 'trends/trend_today_dow.csv', row.names = FALSE)
